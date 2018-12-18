9a52d130eb178143ba5d14cc514680e41cf81a63
Ram Somani <ram.somani36@gmail.com>
2018-12-18 06:05:26 +0530

    modified

---
+++ db/mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+
+module.exports = mongoose;
+
+
+
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
+const mongoose = require('./db/mongo');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
@@ -11,19 +11,11 @@ const users = require('./routes/users');
 const auth = require('./routes/auth');
 const config = require('config');
 
-console.log('CONFIG', config)
 
 if(!config.get('jwtPrivateKey')){
-  console.log(typeof config, config)
   console.log('JwtPrivateKey is not defined');
   process.exit(1);
 }
-
-console.log('ENV', config.get('jwtPrivateKey'))
-
-mongoose.connect('mongodb://localhost:27017/vidly')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
